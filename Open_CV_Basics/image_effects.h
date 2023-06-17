f4b094ccc0634aef48b0656ca67b03b9e428bcd4
CretuAlexandru <49759163+CretuAlexandru7@users.noreply.github.com>
2023-06-17 15:50:17 +0300

    Use copy by reference instead of copy by value

--- Open_CV_Basics/image_effects.h
+++ Open_CV_Basics/image_effects.h
@@ -3,10 +3,10 @@
 
 #include <opencv2/core.hpp>
 
-void vDoInvertColors(cv::Mat frame, int rows, int cols);
-void vDoBinarizeImage(cv::Mat frame, int img_threshold, int rows, int cols);
-void vDoMirrorImage(cv::Mat frame, int rows, int cols);
-void vDoFlipImage(cv::Mat frame, int rows, int cols);
+void vDoInvertColors(cv::Mat& frame, int rows, int cols);
+void vDoBinarizeImage(cv::Mat& frame, int img_threshold, int rows, int cols);
+void vDoMirrorImage(cv::Mat& frame, int rows, int cols);
+void vDoFlipImage(cv::Mat& frame, int rows, int cols);
 void vDoSobelEdgeDetection(const cv::Mat& frame);
 cv::Mat matDoConvertToGrayscale(const cv::Mat& frame);
 cv::Mat matDoCalculateAbsoluteDifference(const cv::Mat& frame1, const cv::Mat& frame2);
